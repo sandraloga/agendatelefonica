b1e54af2d13adb2b28eea96b2214b5f2e95415a2
Sandra <72sandraloga@gmail.com>
2016-03-04 13:38:20 +0100

    Mopdificada clase Test

--- agendatelefonica/TestAgendaTelefonica.java
+++ agendatelefonica/TestAgendaTelefonica.java
@@ -8,26 +8,42 @@
 public class TestAgendaTelefonica
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int contadorFallos;
 
     /**
      * Constructor for objects of class TestAgendaTelefonica
      */
     public TestAgendaTelefonica()
     {
-        // initialise instance variables
-        x = 0;
+        contadorFallos=0;
+        AgendaTelefonica  agenda = new AgendaTelefonica();
+        
+        
+        
+        if (contadorFallos == 0) {
+            System.out.println("Todos los test se han pasado correctamente!!");
+        }
+        else {
+            System.out.println("Han fallado " + contadorFallos + " tests!!!");
+        }
+        
+    }
+    
+    public int testea(Object valorEsperado, Object valorObtenido, 
+                          String nombreFuncion)
+    {
+        int valorDevuelto = 0;
+        if (valorEsperado == valorObtenido) {
+            System.out.println("OK: " + nombreFuncion);
+        }
+        else {
+            System.out.println("ERROR!!!!! " + nombreFuncion);
+            System.out.println("Valor obtenido: " + valorObtenido + 
+                               " / Valor esperado: " + valorEsperado);
+            valorDevuelto = 1;
+        }
+        return valorDevuelto;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+   
 }
