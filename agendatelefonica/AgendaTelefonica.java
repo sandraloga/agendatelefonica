7d0b661bc71ccdb39ae0bd02c6e7f6df6c5ae57a
Sandra <72sandraloga@gmail.com>
2016-03-04 13:06:48 +0100

    Creado nuevo repositorio con dos clases para crear una agenda telefonica . metodos para anadir al HashMap y para consultar.

---
+++ agendatelefonica/AgendaTelefonica.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+
+    private HashMap<String,String> agenda;
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        // initialise instance variables
+        agenda = new HashMap<>();
+    }
+
+    /**
+     * Permite almacenar un nuevo número en la agenda indicando el nombre del contacto
+     * y su numero de telefono asociado
+     */
+    public void enterNumber(String name, String number)
+    {
+        agenda.put(name,number);
+    }
+
+    /**
+     * Devuelve el numero de teléfono del contacto indicado. Si el contacto no existe, 
+     * devuelve null.
+     */
+    public String lookupNumber(String name)
+    {
+        return agenda.get(name);
+    }
+}
